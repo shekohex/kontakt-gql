ac308f3f7be841c8f49386c8a6e8d519cc1b24fb
Shady Khalifa <shekohex@gmail.com>
2018-08-16 17:05:20 +0200

    updated schema

--- src/app/contacts/contacts.resolver.ts
+++ src/app/contacts/contacts.resolver.ts
@@ -1,12 +1,14 @@
 import { Resolver, Query, Mutation } from '@nestjs/graphql';
 import { Context } from '@app/constants';
 import { Contact } from '@app/gql/generated/prisma';
+import { UserInputError } from 'apollo-server-core';
 type Contacts = Contact[];
 
 @Resolver('Contact')
 export class ContactsResolver {
   @Query('contacts')
   async getContacts(_, { limit }, ctx: Context, info): Promise<Contacts> {
+    if (limit > 50) throw new UserInputError('Limit Cannot be greater than 50', { limit });
     return await ctx.db.query.contacts(
       {
         first: limit,
@@ -28,22 +30,30 @@ export class ContactsResolver {
   }
 
   @Mutation()
-  async addContact(_, { data }, ctx: Context, info): Promise<Contact> {
+  async addContact(_, { name, phoneNumber, type }, ctx: Context, info): Promise<Contact> {
     return await ctx.db.mutation.createContact(
       {
-        data,
+        data: {
+          name,
+          phone: {
+            create: {
+              number: phoneNumber,
+              type,
+            },
+          },
+        },
       },
       info,
     );
   }
 
   @Mutation()
-  async updateContact(_, { data, where }, ctx: Context, info): Promise<Contact | null> {
-    return await ctx.db.mutation.updateContact({ data, where }, info);
+  async updateContact(_, { data, id }, ctx: Context, info): Promise<Contact | null> {
+    return await ctx.db.mutation.updateContact({ data, where: { id } }, info);
   }
 
   @Mutation()
-  async deleteContact(_, { where }, ctx: Context, info): Promise<Contact | null> {
-    return await ctx.db.mutation.deleteContact({ where }, info);
+  async deleteContact(_, { id }, ctx: Context, info): Promise<Contact | null> {
+    return await ctx.db.mutation.deleteContact({ where: { id } }, info);
   }
 }
